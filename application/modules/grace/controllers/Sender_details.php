1ec7a171dc269ac0b7f6ec992930e9fa85f4413c
ndutagrace25 <ndutagrace25@gmail.com>
2018-12-19 14:51:04 +0300

    changes

--- application/modules/grace/controllers/Sender_details.php
+++ application/modules/grace/controllers/Sender_details.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Brands extends MX_Controller
+class Sender_details extends MX_Controller
 {
     function __construct() {
 		parent:: __construct();

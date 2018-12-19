729a18726fd11cd9844cf115fda1fa81ab4b44f0
kwizl <karis.njoroge@gmail.com>
2018-12-19 19:41:20 +0300

    updating accesstoken as associated array

--- application/modules/martin/controllers/Martin_card.php
+++ application/modules/martin/controllers/Martin_card.php
@@ -77,8 +77,8 @@ class Martin_card extends MX_Controller
         $ch = curl_init($end_point);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "application_id: ".$application_id,
-            "application_secret: ".$application_secret,
+            "applicationId: ".$application_id,
+            "applicationSecret: ".$application_secret,
             "refreshToken: ".$refresh_token,
             "Content-Type: application/json"
         ));
@@ -86,7 +86,7 @@ class Martin_card extends MX_Controller
         curl_close($ch);
 
         $response_decoded = json_decode($response);
-        return 121;
+        return $response;
 	}
 }
 ?>

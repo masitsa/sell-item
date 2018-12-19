652e6140e47a63cf8fd0a3df0c616ca8f095a26d
kwizl <karis.njoroge@gmail.com>
2018-12-19 11:17:18 +0300

    Martin_card request made

---
+++ application/modules/martin/models/Martin_card_model.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Martin_card_model extends CI_Model
+{
+    function save_card($save_data) {
+        if($this->db->insert("Martin_checkin", $save_data)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

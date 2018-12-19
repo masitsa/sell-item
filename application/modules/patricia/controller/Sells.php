001a3c97bc1249928883d3f5accef0d7246443a0
Muraura <patmur93@gmail.com>
2018-12-19 11:53:32 +0300

    Commit Message

---
+++ application/modules/patricia/controller/Sells.php
@@ -0,0 +1,69 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Sells extends MX_Controller
+{
+    function __construct() {
+		parent:: __construct();
+
+		// Allow from any origin
+		if (isset($_SERVER['HTTP_ORIGIN'])) {
+			header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+			header('Access-Control-Allow-Credentials: true');
+			header('Access-Control-Max-Age: 86400');    // cache for 1 day
+		}
+	
+		// Access-Control headers are received during OPTIONS requests
+		if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+	
+			if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+				header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+	
+			if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+				header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+	
+			exit(0);
+        }
+        
+        $this->load->model("sells_model");
+    }
+
+    function create_sell(){
+        //receive JSON POST
+        $json_string = file_get_contents("php://input");
+        //2.convert JSON to an array
+        $json_object =json_decode($json_string);
+        //3.validate
+        if(is_array($json_object) && (count($json_object)>0)){
+            //retrieve data
+            $row=$json_object[0];
+            $data =array(
+                "Brand_name" =>$row->Brand,
+                "Brand_model_name"=>$row->model,
+                "price"=>$row->price,
+                "color"=>$row->color,
+                "profile_image"=>$row->picture,
+                "responder_name"=>$row->name,
+                "responder_phone"=>$row->phone,
+                "response_time"=>$row->time,
+
+            );
+            //4.Request to submit
+           $save_status= $this->sells_model->save_sell($data);
+           if($save_status ==TRUE){
+               echo"saved";
+           }else{
+               echo"unable to save";
+           }
+
+        }
+        else{
+            //send invalid data message
+            echo"invalid data provided";
+        }
+        //4.Request to save data
+        //5.send a confirmation
+    }
+}
+    
+?>

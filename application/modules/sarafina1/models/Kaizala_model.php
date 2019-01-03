ea33354d6948dba0583891e92f61f5339b38732d
waGachari <sarafinagachari@gmail.com>
2019-01-03 19:19:49 +0300

    kaizala model modified

--- application/modules/sarafina1/models/Kaizala_model.php
+++ application/modules/sarafina1/models/Kaizala_model.php
@@ -101,7 +101,7 @@
             $curl = curl_init();
 
             curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://kms2.kaiza.la/v1/groups/d498658f-2362-46f5-85e6-7f8d8a3e6b75@2/actions",
+            CURLOPT_URL => "https://kms2.kaiza.la/v1/groups/44e43039-2ad9-4b29-ae95-4684ff45a91e@2/actions",
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
@@ -120,12 +120,9 @@
             $err = curl_error($curl);
 
             curl_close($curl);
-            if ($err) {
-            echo "cURL Error #:" . $err;
-            } else {
-                $result_object = json_decode($response);
-                return $result_object->actionId;
-            }
+
+            $result_object = json_decode($response);
+            return $result_object->actionId;
      
              
          }

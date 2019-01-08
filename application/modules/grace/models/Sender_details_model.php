a12aff72447c258598a1269101440aaf81e59794
ndutagrace25 <ndutagrace25@gmail.com>
2019-01-08 15:36:51 +0300

    added the fields

--- application/modules/grace/models/Sender_details_model.php
+++ application/modules/grace/models/Sender_details_model.php
@@ -11,5 +11,34 @@ class Sender_details_model extends CI_Model
         else {
             return FALSE;
         }
+    }  
+    public function get_brand_name($brand_id)
+    {
+        $this->db->where("brand_id", $brand_id);
+        $query = $this->db->get("brand");
+        $brand_name = "";
+
+        if($query->num_rows() > 0)
+        {
+            $row = $query->row();
+            $brand_name = $row->brand_name;
+        }
+
+        return $brand_name;
+    }
+
+    public function get_brand_model_name($brand_model_id)
+    {
+        $this->db->where("brand_model_id", $brand_model_id);
+        $query = $this->db->get("brand_model");
+        $brand_model_name = "";
+
+        if($query->num_rows() > 0)
+        {
+            $row = $query->row();
+            $brand_model_name = $row->brand_model_name;
+        }
+
+        return $brand_model_name;
     }
 }

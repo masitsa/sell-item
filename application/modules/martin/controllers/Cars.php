cc944132d521de30feb8ba948b7cabdd756a9f38
kwizl <karis.njoroge@gmail.com>
2019-01-08 19:14:51 +0300

    Modified car controller

--- application/modules/martin/controllers/Cars.php
+++ application/modules/martin/controllers/Cars.php
@@ -56,7 +56,7 @@ class Cars extends MX_Controller
 
 			// Create announcement receivers
 			$subscribers = array($row->phone);
-			$brand_name = $this->cars_model->get_brand_name($row->brand);
+			$brand_name = $this->cars_model->get_brand_name($row->brand_name);
 			$brand_model_name = $this->cars_model->get_brand_model_name($row->brand_model);
 				
 			$message_fields = array(

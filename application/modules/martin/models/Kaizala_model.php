21fa4b73e6bdb31f4b981375f93c92639d52702a
kwizl <karis.njoroge@gmail.com>
2019-01-08 12:15:35 +0300

    Edited Kaizala model

--- application/modules/martin/models/Kaizala_model.php
+++ application/modules/martin/models/Kaizala_model.php
@@ -27,7 +27,7 @@ class Kaizala_model extends CI_Model
         return $response_decoded->accessToken;
     }
     
-    public function send_announcement($title, $$description, $status, $date, $fields, $receivers)
+    public function send_announcement($title, $description, $status, $date, $fields, $receivers)
     {
         $group_id = "a2648ba2-927d-4c95-a78b-a6dc473fe6f5@2";
         $url = "https://kms2.kaiza.la/v1/groups/".$group_id."/actions";

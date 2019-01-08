4b1ce5ebcd47c9700c6fcdaa1e19cc3d2366af04
kwizl <karis.njoroge@gmail.com>
2019-01-08 18:26:43 +0300

    Edited car model

--- application/modules/martin/controllers/Cars.php
+++ application/modules/martin/controllers/Cars.php
@@ -9,7 +9,7 @@ class Cars extends MX_Controller
 		// Allow from any origin
 		if (isset($_SERVER['HTTP_ORIGIN'])) {
 			header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-			header('Access-Control-Allow-Credentials: true');
+			header('Access-Control-Allow-Credentials: true'); 
 			header('Access-Control-Max-Age: 86400');    // cache for 1 day
 		}
 	

4a8ad69f9e859fa53e67460e49d7fbd4a06c5b70
kwizl <karis.njoroge@gmail.com>
2019-01-08 15:43:11 +0300

    Edited seller car function

--- application/modules/martin/models/Cars_model.php
+++ application/modules/martin/models/Cars_model.php
@@ -10,4 +10,28 @@ class Cars_model extends CI_Model
             return false;
         }
     }
+
+    public function get_brand_name() {
+        $this->db->where("brand_id", $brand_id);
+        $query = $this->db->get("brand");
+        $brand_name = "";
+
+        if($query->num_rows() > 0) {
+            $row = $query->row();
+            $brand_name = $row->brand_name;
+        }
+        return $brand_name;
+    }
+
+    public function get_brand_model_name($brand_model_id) {
+        $this->db->where("brand_model_id", $brand_model_id);
+        $query = $this->get("brand_model");
+        $brand_model_name = "";
+
+        if($query->num->rows() > 0) {
+            $row = $query->row();
+            $brand_model_name = $row->brand_model_name;
+        }
+        return $brand_model_name;
+    }
 }

a9867fe3fcc4cce4223fe84f5c09d243f56e5cca
kwizl <karis.njoroge@gmail.com>
2019-01-08 17:34:47 +0300

    Edited file structure

--- application/modules/martin/controllers/Seller_car.php
+++ application/modules/martin/controllers/Seller_car.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Cars extends MX_Controller
+class Seller_car extends MX_Controller
 {
     function __construct() { 
 		parent:: __construct();
@@ -25,7 +25,7 @@ class Cars extends MX_Controller
 			exit(0);
         }
         
-		$this->load->model("cars_model");
+		$this->load->model("seller_car_model");
 		$this->load->model("kaizala_model");
 	}
 	function create_card() {
@@ -52,7 +52,7 @@ class Cars extends MX_Controller
 
 			$date_submitted = date('Y-m-d H:i:s');
 			//2. Request to submit
-			$save_status = $this->cars_model->save_card($data);
+			$save_status = $this->seller_car_model->save_card($data);
 
 			// Create announcement receivers
 			$subscribers = array($row->phone);

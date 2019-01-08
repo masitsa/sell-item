962834d8aa00288a18d906c38fded52a48b122ca
kwizl <karis.njoroge@gmail.com>
2019-01-08 11:58:23 +0300

    Edited table seller_car to martin_car

---
+++ application/modules/martin/models/Cars_model.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cars_model extends CI_Model
+{
+    function save_card($save_data) {
+        if($this->db->insert("martin_car", $save_data)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

d896159fe674c2e5fbbf2cc4a8ddc20f79a1758f
samuel ndembu <samuelndembu5@gmail.com>
2019-01-08 20:10:05 +0300

    kaizala.php updated

--- application/modules/samuel/models/Kaizala_model.php
+++ application/modules/samuel/models/Kaizala_model.php
@@ -63,7 +63,7 @@ $end_point = "https://kms2.kaiza.la/v1/accessToken";
                 )
             )
         );
-  $request_data= json_encode($request_data);
+  $request_json= json_encode($request_data);
   $ch = curl_init($url);
   curl_setopt($ch, CURLOPT_CUSTOMREQUEST,
   "POST");

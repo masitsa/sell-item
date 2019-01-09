ffdbbefb79d0d11a9a33d400772326d2768ee44f
samuel ndembu <samuelndembu5@gmail.com>
2019-01-09 16:02:20 +0300

    kaizala_model.php updated

--- application/modules/samuel/models/Kaizala_model.php
+++ application/modules/samuel/models/Kaizala_model.php
@@ -32,7 +32,7 @@ class Kaizala_model extends CI_Model
         $access_token = $this->get_access_token();
         // echo $access_token."<br/>";
         $request_data = array(
-            "id" => "com.nanyukiaf.samuel.car.announcement.20",
+            "id" => "com.nanyukiaf.samuel.car.announcement.21",
             "sendToAllSubscribers" => false,
             "subscribers" => $receivers,
             "actionBody" => array(

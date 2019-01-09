034391869a06eaa42ecadf7d7080aeea3f97e184
waGachari <sarafinagachari@gmail.com>
2019-01-09 18:42:06 +0300

    announcement id changed

--- application/modules/sarafina1/models/Kaizala_model.php
+++ application/modules/sarafina1/models/Kaizala_model.php
@@ -36,7 +36,7 @@ class Kaizala_model extends CI_Model
         $accessToken =$this->get_access_token();
 
         $request_data = array(
-            "id"=>"com.nanyukiaf.sarafina.car.announcement.24",
+            "id"=>"com.nanyukiaf.sarafina.car.announcement.25",
             "sendToAllSubscribers"=>false,
             "subscribers"=>$receivers,
             "actionBody"=>array(

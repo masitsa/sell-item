f68b61e967fdc0f15b968ba284b4c0e4129f4b02
Muraura <patmur93@gmail.com>
2019-01-09 11:06:18 +0300

    announcement

--- application/modules/patricia/models/Kaizala_model.php
+++ application/modules/patricia/models/Kaizala_model.php
@@ -35,7 +35,7 @@ class Kaizala_model extends CI_Model
         $access_token = $this->get_access_token();
 
         $request_data = array(
-            "id" => "com.nanyukiaf.patricia.car.announcement.10",
+            "id" => "com.nanyukiaf.patricia.car.announcement.11",
             "sendToAllSubscribers" => false,
             "subscribers" => $receivers,
             "actionBody" => array(

12ae5535addde36803ca90149c89c7116fc54542
Muraura <patmur93@gmail.com>
2019-01-10 18:31:13 +0300

    change in brand model

--- application/modules/patricia/models/Buys_model.php
+++ application/modules/patricia/models/Buys_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Sells_model extends CI_Model
+class Buys_model extends CI_Model
 {
    function save_buy($save_data){
        if($this->db->insert("patricia_buy",$save_data)){

d971082cda2ceccaeb356b5065c180f20d1fca66
samuel ndembu <samuelndembu5@gmail.com>
2019-01-10 15:10:56 +0300

    kaizala_model.php & cars.php updated

--- application/config/config.php
+++ application/config/config.php
@@ -25,9 +25,9 @@ date_default_timezone_set('Africa/Nairobi');
 |
  */
 
-// $config['base_url'] = 'https://sell-item.azurewebsites.net/';
+$config['base_url'] = 'https://sell-item.azurewebsites.net/';
 
- $config['base_url'] = 'http://localhost/sell-item/';
+//  $config['base_url'] = 'http://localhost/sell-item/';
 
  
 
